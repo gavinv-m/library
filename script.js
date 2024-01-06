9021581a45778d1883cc1b6629188ba35a59c055
Gavin <gavinvm@protonmail.com>
2024-01-06 16:40:03 +0200

    Popup form when add button clicked

---
+++ script.js
@@ -0,0 +1,6 @@
+const addButton = document.getElementById('add-book');
+const dialog = document.getElementById('dialog');
+
+addButton.addEventListener('click', () => {
+    dialog.showModal();
+});
